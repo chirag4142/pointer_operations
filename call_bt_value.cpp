8c49bf8fc577bc33f4facbc5de73832de56a2809
chirag tekwani <ctekwani051@gmail.com>
2024-09-04 11:45:46 +0530

    Create call_bt_value.cpp

---
+++ call_bt_value.cpp
@@ -0,0 +1,25 @@
+//Name:Chirag Tekwani
+//PRN: 23070123042
+//Aim: To use call by value.
+#include <iostream>
+#include<string>
+using namespace std;
+
+void swap(int x , int y){
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
+int main() {
+    int a = 5, b = 2;
+    swap(a,b);
+    cout << "Value of the a is:" << a<<endl;
+    cout << "Value of the b is:" << b<<endl;
+    return 0;
+}
+
+/* OUTPUT
+Value of the a is:5
+Value of the b is:2
+*/
